51c8811447a09a82c8b8947a4b06970836e73778
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-06 19:14:42 +0530

    ec2 remote exe creation

---
+++ ec2.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "ansible-master-ec2" {
+  ami           = "ami-09c813fb71547fc4f" 
+  vpc_security_group_ids = ["sg-06b1b57b365846051"]
+
+  instance_type = "t3.micro"
+
+    connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+  
+   provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install ansible -y"
+    ]
+  }
+   tags = {
+    Name = "ansible-master" #Here 'Helloworld-db' is the ec2-instance name in AWS, not related to Terrafrom.
+  }
+}
+
+
+
+# resource "aws_instance" "ansible-node1" {
+#   ami           = "ami-09c813fb71547fc4f" 
+#   vpc_security_group_ids = ["sg-06b1b57b365846051"]
+
+#   instance_type = "t2.micro"
+
+#   #   connection {
+#   #   type     = "ssh"
+#   #   user     = "ec2-user"
+#   #   password = "DevOps321"
+#   #   host     = self.public_ip
+#   # }
+  
+#   #  provisioner "remote-exec" {
+#   #   inline = [
+#   #     "sudo dnf install ansible -y"
+#   #   ]
+#   # }
+#    tags = {
+#     Name = "ansible-node1" #Here 'Helloworld-db' is the ec2-instance name in AWS, not related to Terrafrom.
+#   }
+# }
